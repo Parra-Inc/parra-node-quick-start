ac1d21414f6213f588023e27bcee62e366dedabd
Mick MacCallum <micksmaccallum@gmail.com>
2022-04-30 14:47:31 -0400

    formatting improvements

--- app.js
+++ app.js
@@ -1,44 +1,47 @@
-const assert = require("assert")
-const express = require("express")
-const { AccessToken } = require('@parra/access-token')
+const assert = require("assert");
+const express = require("express");
+
+const { AccessToken } = require("@parra/access-token");
 
 // TODO: - Replace with your keys
 const parraTenantId = process.env.PARRA_TENANT_ID;
 const parraApiKeyId = process.env.PARRA_API_KEY_ID;
 const parraApiKeySecret = process.env.PARRA_API_SECRET;
 
-assert(parraTenantId, parraApiKeyId, parraApiKeySecret)
+assert(parraTenantId, parraApiKeyId, parraApiKeySecret);
 
-const app = express()
+const app = express();
 
 // TODO: - Replace with your authentication middleware
 const authenticationMiddleware = (req, res, next) => {
-    // Fake auth
-    req.user = {
-        id: "not-a-real-user-id"
-    }
+  // Fake auth
+  req.user = {
+    id: "not-a-real-user-id",
+  };
 
-    next()
-}
+  next();
+};
 
-app.use(authenticationMiddleware)
+app.use(authenticationMiddleware);
 
 app.post("/v1/parra/auth/token", (req, res) => {
-    // TODO: - Extract user id from request using your authentication method
-    const userId = req.user.id
+  // TODO: - Extract user id from request using your authentication method
+  const userId = req.user.id;
 
-    // Create an access token which we will sign and return to the client
-    const token = new AccessToken(
-        parraTenantId,
-        parraApiKeyId,
-        parraApiKeySecret,
-        { identity: userId }
-    );
+  console.log(`Generating access token for user: ${userId}`);
 
-    // Create a JWT from the Access Token
-    res.json({
-        access_token: token.toJwt()
-    })
-})
+  // Create an access token which we will sign and return to the client
+  const token = new AccessToken(
+    parraTenantId,
+    parraApiKeyId,
+    parraApiKeySecret,
+    { identity: userId }
+  );
 
-module.exports = app
+  // Create a JWT from the Access Token
+  res.json({
+    access_token: token.toJwt(),
+  });
+});
+
+module.exports = app;
