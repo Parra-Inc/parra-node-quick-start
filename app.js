f38409800087bc383a451649f3e50d9033b74444
Mick MacCallum <micksmaccallum@gmail.com>
2024-08-06 11:22:44 -0400

    use passed uuid to identify user

--- app.js
+++ app.js
@@ -12,11 +12,25 @@ assert(parraTenantId, parraApiKeyId, parraApiKeySecret);
 
 const app = express();
 
-// TODO: - Replace with your authentication middleware
 const authenticationMiddleware = (req, res, next) => {
+  const token = req.headers['Authorization'];
+
+  if (!token) {
+    const err = new Error('Authorization header missing');
+    err.statusCode = 401; // Unauthorized
+
+    return next(err);
+  }
+
+  // To keep the example simple, the app is just sending a user id as the authorization
+  // header. Normally, you would use your app's own authorization API to obtain an access
+  // token. This access token would be passed as the authorization header to this
+  // endpoint, and would be decoded to access the user id.
+  const [_, userId] = token.split(' ');
+
   // Fake auth
   req.user = {
-    id: "not-a-real-user-id",
+    id: userId,
   };
 
   next();
@@ -25,7 +39,6 @@ const authenticationMiddleware = (req, res, next) => {
 app.use(authenticationMiddleware);
 
 app.post("/v1/parra/auth/token", (req, res) => {
-  // TODO: - Extract user id from request using your authentication method
   const userId = req.user.id;
 
   console.log(`Generating access token for user: ${userId}`);
